1cde8f31950253fce636fb0d95f4014678031511
Edwin Tang <edwintang10@gmail.com>
2024-03-22 20:53:18 +0800

    reduce width of text chunk. add profile page.

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
         <p className="text-center text-white"> 無限 </p>
       </div>
 
-      <div className="relative flex place-items-center">
+      <div className="relative flex place-items-center w-9/12">
         <p className="font-mono text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum elit ut dui accumsan, eu facilisis erat luctus. Maecenas sed justo id neque tempus tristique quis nec purus. Donec vitae turpis gravida, elementum felis eu, eleifend tellus. Integer pulvinar est et neque luctus iaculis. Proin id consectetur lectus. Nam dignissim malesuada felis, vitae scelerisque justo aliquam sit amet. Maecenas eu posuere diam. Curabitur suscipit auctor felis, quis lobortis magna volutpat quis. Nulla quis magna a elit pharetra finibus. Proin mattis justo vel lacinia euismod.</p>
       </div>
 
