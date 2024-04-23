86f2098280e9efb25e22bd8755f974c91bcd9c40
TanglingTreats <tanglingtreats69@gmail.com>
2024-04-23 16:25:59 +0800

    feat: add other colors of gruvbox into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,11 @@ module.exports = {
         'black': '#282828',
         'yellow': '#d79921',
         'blue': '#458588',
-        'red': '#fb4934',
+        'red': '#cc241d',
+        'blue': '#458588',
+        'purple': '#b16286',
+        'aqua': '#689d6a',
+        'gray': '#a89984'
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
